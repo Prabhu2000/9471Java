2e2f8f4c9fb95b4ac8af53a679553f8062690357
Prabhu2000 <95089893+Prabhu2000@users.noreply.github.com>
2021-12-08 19:58:11 +0530

    Create Array2.java

---
+++ Anudip practical/Array2.java
@@ -0,0 +1,14 @@
+
+
+Solution-->
+
+public class Array2{
+public static void main(String[] args){
+int b[]={7,8,5,6,7,8};
+int sum=0;
+for(int i=0;i<b.lenth;i++){
+sum += b[i];
+}
+System.out.println("sum of array" + sum);
+}
+}
