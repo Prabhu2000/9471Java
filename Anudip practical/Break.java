0a27451df9714d29425350a9051a4225e8190434
Prabhu2000 <95089893+Prabhu2000@users.noreply.github.com>
2021-12-09 18:39:16 +0530

    Create Break.java

---
+++ Anudip practical/Break.java
@@ -0,0 +1,14 @@
+class Break{
+public static void main(String[] args){
+aa:
+for(int i=0;i<=5;i++){
+bb:
+for(int j=0;j<=6;j++){
+if(j==2 && j==2){
+break;
+}
+System.out.println(+i""+j);
+}
+}
+}
+}
