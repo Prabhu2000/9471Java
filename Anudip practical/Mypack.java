82aedc745fe0220841555cdd9f5b0a136e5f9de5
Prabhu2000 <95089893+Prabhu2000@users.noreply.github.com>
2021-12-09 19:00:23 +0530

    Create Mypack.java

---
+++ Anudip practical/Mypack.java
@@ -0,0 +1,12 @@
+Package Mypack;
+class A{
+protected String name ="Aditya";
+Protected void msg(){
+System.out.println("Hello java");}
+}
+class Mypack extends A{
+public static void main(String[] args)
+Mypack obj = new Mypack();
+obj.msg();
+}
+}
