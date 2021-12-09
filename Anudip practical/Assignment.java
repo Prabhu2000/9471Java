5d8a208ae97ccd1688194adbede6397e182623d7
Prabhu2000 <95089893+Prabhu2000@users.noreply.github.com>
2021-12-09 18:31:44 +0530

    Create Assignment.java

---
+++ Anudip practical/Assignment.java
@@ -0,0 +1,12 @@
+class Assignment{
+public static void main(String[] args){
+int a=20;
+int b=100;
+b=a;
+System.out.println("output:" +b);
+b+=a;
+System.out.println("output:" +b);
+b*=a;
+System.out.println("output:" +b);
+}
+}
