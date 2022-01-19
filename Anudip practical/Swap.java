6646b869d5b014c94f54ebe81279bcb6aff56b59
Prabhu2000 <95089893+Prabhu2000@users.noreply.github.com>
2022-01-19 15:25:26 +0530

    Create Swap.java

---
+++ Anudip practical/Swap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Swap {
+	public static void main(String[] args) {
+		int a,b,t;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your two number");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println("before sewaping"+a+" "+b);
+		t=a;
+		a=b;
+		b=t;
+		System.out.println("after swaping="+a+" "+b);
+	}
+	
+
+}
